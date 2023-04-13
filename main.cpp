df8dd1c57d58eee21c2c1c2e0cadb57b5d46d915
krc2000 <pomkov2000@gmail.com>
2023-04-13 20:15:26 +0300

    Cleaning

--- main.cpp
+++ main.cpp
@@ -5,19 +5,14 @@
 #include <imgui.h>
 #include <imgui-SFML.h>
 
-#include "Camera.h"
-#include "Marker.h"
-
 int main() {
     sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
-    window.setFramerateLimit(60);
-    ImGui::SFML::Init(window);
-    
-    Camera cam({300.f, 200.f});
-
-    Marker m1({150.f, 50.f}), m2({100.f, 100.f});
-
-    bool viewScrolled = false;
+    window.setFramerateLimit(30);
+    if (!ImGui::SFML::Init(window))
+    {
+        printf("Failed to initialize ImGui!");
+        return 1;
+    }
 
     sf::Clock deltaClock;
     while (window.isOpen()) 
@@ -26,51 +21,20 @@ int main() {
         while (window.pollEvent(event))
         {
             ImGui::SFML::ProcessEvent(window, event);
-            cam.updateEvent(event);
 
             if (event.type == sf::Event::Closed) 
             {
                 window.close();
             }
-            if (event.type == sf::Event::KeyPressed)
-            {
-                if (event.key.code == sf::Keyboard::LAlt)
-                    viewScrolled = true;
-            }
-            if (event.type == sf::Event::KeyReleased)
-            {
-                if (event.key.code == sf::Keyboard::LAlt)
-                    viewScrolled = false;
-            }
-            if (event.type == sf::Event::MouseButtonPressed)
-            {
-                //if (event.mouseButton.button == sf::Mouse::Middle)
-                     // viewScrolled = true;
-            }
-            if (event.type == sf::Event::MouseButtonReleased)
-            {
-                // if (event.mouseButton.button == sf::Mouse::Middle)
-                //     viewScrolled = false;
-            }
-        }
-
-        if (viewScrolled)
-        {
-            
         }
 
         ImGui::SFML::Update(window, deltaClock.restart());
-        cam.update(window);
 
         ImGui::Begin("Hello, world!");
         ImGui::Button("Look at this pretty button");
         ImGui::End();
 
-        window.setView(cam.getView());
         window.clear();
-
-        window.draw(m1);
-        window.draw(m2);
 
         ImGui::SFML::Render(window);
         window.display();
